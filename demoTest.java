c9734f73f620cc4de70f5328c2585477ba29dead
jonidervishi <joni.dervishi@gmail.com>
2022-03-16 15:38:26 -0700

    fixed test to make it work

--- demoTest.java
+++ demoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class demoTest {
     @Test
     public void subtraction() {
-        assertEquals(5, demo.subtract(4, 1));
+        assertEquals(3, demo.subtract(4, 1));
     }
 }
